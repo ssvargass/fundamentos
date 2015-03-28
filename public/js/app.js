9af424e856bd22e0110b7bd89de8e085c5448cfa
SsvargasS <sergio.svargass@gmail.com>
2015-03-27 20:46:28 -0500

    Calcula valores

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,56 @@
 (function () {
-  var app = angular.module('fundamentosController', []);
+  var app = angular.module('fundamentosController', ['ng-currency']);
 
   app.controller('TasasEquivalentesController', ['$scope', '$interval', '$http',
     function ($scope, $interval, $http) {
-      
-    $scope.n = [
+    // $scope.interes = 0;
+    $scope.selected = 0;
+    
+    $scope.liquidada = "";
+    $scope.efectiva = "";
+    $scope.nOptions = [
       {
         value : 12,
         liquidada : 'mes',
-
+        efectiva : 'mensual'
       },
       {
         value : 6,
         liquidada : 'bimestre',
-
+        efectiva : 'bimestral'
       },
       {
         value : 4,
         liquidada : 'trimestre',
+        efectiva : 'trimestral'
       },
       {
         value : 2,
-        liquidada : 'semestre'
+        liquidada : 'semestre',
+        efectiva : 'semestral'
       },
     ];
 
+    $scope.n = $scope.nOptions[0];
+
+    $scope.getN = function(index){
+      $scope.n = $scope.nOptions[index];
+      $scope.selected = index;
+      $scope.getIem();
+    } 
+
+    $scope.getIem = function(){
+      $scope.iem = $scope.interes / $scope.n.value;
+      $scope.liquidada = "liquidada " +$scope.n.liquidada+ " vencido";
+      $scope.efectiva = "Tasa de interés efectiva " +$scope.n.efectiva;
+      $scope.printalert = true;
+      $scope.getFinal();
+    }
     
-    $scope.Final = $scope.capital * Math.pow(( 1 + $scope.ieb), $scope.n);
+    $scope.getFinal = function(){
+      $scope.Final = $scope.capital * Math.pow(( 1 + $scope.iem/100), $scope.n.value);
+    } 
+
 
 
   }]);
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 (function ($) {
+	$(document).ready(function(){
+		$('#interes').tooltip({'trigger':'hover', 'title': 'Password tooltip'});	
+	})
 	
-
 })(jQuery);
