8c676bb8a63ef7aecb02d419dd07b3dc20bf7816
SsvargasS <sergio.svargass@gmail.com>
2015-03-28 03:08:48 -0500

    Simulador terminado

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,10 @@
 
   app.controller('TasasEquivalentesController', ['$scope', '$filter',
     function ($scope, $filter) {
-    // $scope.interes = 0;
     $scope.selected = 0;
+    $scope.sselected = 0;
     $scope.rows = [];
+    $scope.srows = [];
     $scope.liquidada = "";
     $scope.efectiva = "";
     $scope.nOptions = [
@@ -32,6 +33,7 @@
     ];
 
     $scope.n = $scope.nOptions[0];
+    $scope.sn = $scope.nOptions[0];
 
     $scope.getN = function(index){
       $scope.n = $scope.nOptions[index];
@@ -39,29 +41,58 @@
       $scope.getIem();
     } 
 
+    $scope.getNs = function(index){
+      $scope.sn = $scope.nOptions[index];
+      $scope.sselected = index;
+      $scope.getInteres();
+    } 
+
     $scope.getIem = function(){
       $scope.iem = $scope.interes / $scope.n.value;
       $scope.liquidada = "Tasa de interes Nominal de " + $scope.interes +"% - liquidada " +$scope.n.liquidada+ " vencido";
-      $scope.efectiva = $scope.iem +"% - Tasa de interés efectiva " +$scope.n.efectiva;
+      $scope.efectiva = $scope.iem + "% - Tasa de interés efectiva " +$scope.n.efectiva;
       if($scope.interes != undefined) $scope.printalert = true;
       $scope.getFinal();
+      $scope.getInteres();
+      $scope.getIea();
+    }
+
+    $scope.getIea = function(){
+      $scope.iea = Math.pow(1 + ($scope.interes/100) / $scope.n.value , $scope.n.value) - 1;
+      $scope.iea *= 100;
     }
     
     $scope.getFinal = function(){
       $scope.Final = $scope.getFinalN($scope.n.value, $scope.capital, $scope.iem);
-      $scope.getTable();
+      $scope.rows = [];
+      $scope.rows = $scope.getTable($scope.n.value, $scope.capital, $scope.iem);
+      $scope.getInteres();
     } 
 
-    $scope.getTable = function(){
-      $scope.rows = [];
-      for(var i = 0; i <= $scope.n.value; i++){
+    $scope.getInteres = function(){
+      $scope.sinteres = (Math.pow(1 + ($scope.interes/100) / $scope.n.value , $scope.n.value/$scope.sn.value) - 1) * $scope.sn.value;
+      $scope.sinteres *= 100;
+      $scope.siem = $scope.sinteres / $scope.sn.value;
+      $scope.sFinal = $scope.getFinalN($scope.sn.value, $scope.capital, $scope.siem);
+      $scope.srows = [];
+      $scope.srows = $scope.getTable($scope.sn.value, $scope.capital, $scope.siem);
+      var inter = $filter('number')($scope.sinteres,2);
+      var siem = $filter('number')($scope.siem, 2);
+      $scope.sliquidada = "Tasa de interes Nominal de " + inter +"% - liquidada " +$scope.sn.liquidada+ " vencido";
+      $scope.sefectiva = siem + "% - Tasa de interés efectiva " +$scope.sn.efectiva;
+    }
+
+
+    $scope.getTable = function(n,c,iem){
+      var rows = [];
+      for(var i = 0; i <= n; i++){
         var interes = 0;
         var deposito = '';
-        var futuro = $scope.getFinalN(i, $scope.capital, $scope.iem);
+        var futuro = $scope.getFinalN(i, c, iem);
         if(i > 0){
-          interes = futuro * $scope.iem/100;
+          interes = futuro * iem/100;
         } else {
-          deposito = $filter('currency')($scope.capital);
+          deposito = $filter('currency')(c);
         }
         
         var row = {
@@ -70,8 +101,9 @@
           interes : interes,
           futuro : futuro,
         }
-        $scope.rows.push(row);
+        rows.push(row);
       }
+      return rows
     }
 
     $scope.getFinalN = function(n,c,iem){
@@ -81,4 +113,30 @@
 
   }]);
 
+  app.directive('numberFormat', ['$filter', '$parse', function ($filter, $parse) {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelController) {
+            var decimals = $parse(attrs.decimals)(scope);
+
+            ngModelController.$parsers.push(function (data) {
+                //convert data from view format to model format
+                return $filter('number')(data, decimals); //converted
+            });
+
+            ngModelController.$formatters.push(function (data) {
+                //convert data from model format to view format
+                return $filter('number')(data, decimals); //converted
+            });
+
+            element.bind('focus', function () {
+                element.val(ngModelController.$modelValue);
+            });
+
+            element.bind('blur', function () {
+                element.val(ngModelController.$viewValue);
+            });
+        }
+    }
+  }]);
 })();
